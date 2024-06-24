1a935e1285ae3730fba0bc01f07b9996dde6930c
mark <markfirn@gmail.com>
2024-06-24 14:48:47 +0200

    Add iterative merge sort to benchmark

--- sort_speed_test/src/main.rs
+++ sort_speed_test/src/main.rs
@@ -1,4 +1,5 @@
 #![allow(unused_imports)]
+#![allow(dead_code)]
 use std::io::{self, BufRead};
 use rand::prelude::*;
 use std::time::Instant;
@@ -14,9 +15,10 @@ fn main() {
         let mut right = 10000;
         for _ in 0..10 {
             let size = left + (right - left) / 2;
-            let mut data: Vec<i32> = (0..size).map(|_| rng.gen_range(0..1000)).collect();
+            let data: Vec<i32> = (0..size).map(|_| rng.gen_range(0..1000)).collect();
             let time_insertion = measure_sort(&mut data.clone(), insertion_sort);
             let time_merge = measure_sort(&mut data.clone(), merge_sort);
+            //let time_merge = measure_sort(&mut data.clone(), merge_sort_iterative);
 
             if time_insertion <= time_merge {
                 left = size;
@@ -39,6 +41,45 @@ where
     let start = Instant::now();
     sort_function(data);
     start.elapsed().as_micros()
+}
+
+fn merge_sort_iterative(nums: &mut [i32]) {
+    let n = nums.len();
+    let mut size_power = 0;
+    let mut size = 2_i32.pow(size_power) as usize;
+
+    // for window size in powers of two
+    while size <= n {
+        let mut start = 0;
+        // for every window
+        while start < n {
+            let right = (start + 2 * size).min(n);
+            let pivot = (start + size).min(n);
+            merge_helper(nums, start, right, pivot);
+            start += 2 * size;
+        }
+
+        size_power += 1;
+        size = 2_i32.pow(size_power) as usize;
+    }
+}
+
+fn merge_helper(nums: &mut [i32], left: usize, right: usize, pivot: usize) {
+    let aux = nums[left..pivot].to_vec(); // Optimization #2: half-size aux array
+    let mut r_a = 0; // reader aux
+    let mut r_n = pivot; // reader nums
+    let mut w = left; // writer nums
+
+    while r_a < aux.len() || r_n < right {
+        if r_n >= right || (r_a < aux.len() && aux[r_a] <= nums[r_n]) {
+            nums[w] = aux[r_a];
+            r_a += 1;
+        } else {
+            nums[w] = nums[r_n];
+            r_n += 1;
+        }
+        w += 1;
+    }
 }
 
 fn merge_sort(nums: &mut [i32]) {
